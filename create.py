aad09b9424cacbc208a1cebf224b7b7eb905bb3b
Elio Campitelli <eliocampitelli@gmail.com>
2025-12-17 13:59:21 +1100

    Creates operations

--- create.py
+++ create.py
@@ -1,47 +1,6 @@
 import os
-
-OPERATORS = [
-    {
-        "command": "ymonmean",
-        "n_input": 1,
-        "n_output": 1,
-        "params": {}
-    },
-    {
-        "command": "sub",
-        "n_input": 2,
-        "n_output": 1,
-        "params": {}
-    },
-    {
-        "command": "mergetime",
-        "n_input": float("inf"),
-        "n_output": 1,
-        "params": {}
-    },
-    {
-        "command": "remapbil",
-        "n_input": 1,
-        "n_output": 1,
-        "params": {
-            "grid": "grid to remap to"
-            }
-    },
-    {
-        "command": "sellonlatbox",
-        "n_input": 1,
-        "n_output": 1,
-        "params": {
-            "lon1": "left longitude",
-            "lon2": "right longitude",
-            "lat1": "lower latitude",
-            "lat2": "upper latitude"
-        }
-    }
-]
-
-
 import json
+
 
 # n_input or n_output can be "Inf", parse it correctly
 def inf_hook(obj):
@@ -56,9 +15,13 @@ with open('operators.json', 'r') as file:
 OPERATORS = list(OPERATORS.values())
 
 
-
 def make_arguments(op):
-    params = list(op["params"].keys())
+    # For now arguments are all "optional". 
+    if not op["params"]:
+        params = []
+    else:
+        params = list(op["params"]["optional"].keys())
+
     extra_files = []
     if op["n_input"] != float("inf"):
         extra_files = [f"ifile{i}" for i in range(2, int(op["n_input"]) + 1)]
@@ -68,34 +31,33 @@ def make_docstring(op):
     if not op["params"]:
         return f"CDO operator: {op['command']}"
     lines = [f"CDO operator: {op['command']}", "Parameters:"]
-    for k, v in op["params"].items():
+    for k, v in op["params"]["optional"].items():
         lines.append(f"    {k}: {v}")
     return "\n".join(lines)
 
-TEMPLATE = '''
-from ..cdo_operator import CdoOperator
-inf=float("inf")
-def {command}({signature}):
-    r"""
-    {docstring}
-    """
-    operator = CdoOperator(command="{command}",
-                           n_input={n_input}, 
-                           n_output={n_output}, 
-                           params={params_list})
-    return self._new_op(operator, {files_list}, {params_dic})
+
+
+TEMPLATE='''
+    def {fun_name}({signature}):
+        r"""
+        {docstring}
+        """
+        operator = CdoOperator(command="{command}",
+                               n_input={n_input}, 
+                               n_output={n_output}, 
+                               params={params_list})
+                               
+        return self._new_op(operator, {files_list}, {params_dic})
 '''
 
-out_dir="pycdo/operators"
-all = []
-
-import os
-os.system("rm -r pycdo/operators/*")
-
+all_ops = []
 
 for op in OPERATORS:
+    fun_name = op["command"]
+
+    # "not" is reserved word so we can't use it as a function name :(
     if (op["command"] == "not"):
-        op["command"] = "negate"
+        fun_name = "negate"
         
     print(op["command"])
     extra_files, params = make_arguments(op)
@@ -103,9 +65,10 @@ for op in OPERATORS:
     files_list = "[" + ", ".join(extra_files) + "]"
     command = op["command"]
 
-    all.append(f"from .{command} import {command}")
-
+    # Make all arguments optional
+    params_default = [param + " = None" for param in params]
     code = TEMPLATE.format(
+        fun_name = fun_name,
         command = op["command"],
         docstring = make_docstring(op),
         n_input = op["n_input"],
@@ -113,13 +76,29 @@ for op in OPERATORS:
         params_list = str(params),
         params_dic = params_dic,
         files_list = files_list,
-        signature = "self, " + ", ".join(extra_files + params)
+        signature = ", ".join(["self"] + extra_files + params_default)
     )
     
+    all_ops.append(code)
 
-    with open(os.path.join(out_dir, f"{command}.py"), "w") as f: 
-        f.write(code)
 
-    
-with open(os.path.join(out_dir, "__init__.py"), "w") as f: 
-    f.write("\n".join(all))
+
+operation_file = "pycdo/cdo_operation.py"
+marker = "## <<start operators>>"
+
+with open(operation_file, "r") as f:
+    lines = []
+    for line in f:
+        lines.append(line)
+        if "## <<start operators>>" in line:
+            break
+
+
+
+code = "".join(lines) + "".join(all_ops)
+
+
+with open(operation_file, "w") as f: 
+    f.write(code)
+
+
